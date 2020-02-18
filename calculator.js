4549f266f5883bf0eae4c84ee405bbd00097c249
NazarSh <nazarshinkarenko.l@gmail.com>
2020-02-18 16:32:21 +0000

    add js

---
+++ calculator.js
@@ -0,0 +1,55 @@
+
+function calculateTip() {
+  var amountB = document.getElementById("amountB").value;
+  var chooseOption = document.getElementById("chooseOption").value;
+  var amountPeople = document.getElementById("amountPeople").value;
+
+  var result= (amountB * chooseOption) / amountPeople;
+  result = Math.round(result * 100) / 100;
+  result = result.toFixed(2);
+  document.getElementById("totalTip").innerHTML = result;
+}
+
+document.getElementById("calculate").onclick = function() {
+  calculateTip();
+
+};
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+/*  //Display the tip
+  document.getElementById("totalTip").style.display = "block";
+  document.getElementById("tip").innerHTML = total;
+
+}
+
+//Hide the tip amount on load
+document.getElementById("totalTip").style.display = "none";
+document.getElementById("each").style.display = "none";*/ 
